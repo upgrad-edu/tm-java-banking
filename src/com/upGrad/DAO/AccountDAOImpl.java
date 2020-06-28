7d209f4c85a95ade5d186169510760d42b9fa460
sndpoffcl <sandeep2official@gmail.com>
2020-06-29 04:49:39 +0530

    adding code stub for session2

---
+++ src/com/upGrad/DAO/AccountDAOImpl.java
@@ -0,0 +1,28 @@
+package com.upGrad.DAO;
+
+import com.upGrad.beans.Account;
+
+import java.util.HashMap;
+
+public class AccountDAOImpl {
+
+    public Account acceptAccountDetails(Account account) {
+        System.out.println("code for saving account object in the database");
+        return null;
+    }
+
+    public Account getAccountDetails(int accountNumber) {
+        System.out.println("code for getting the account object from database using the accountNumber");
+        return null;
+    }
+
+    public Account updateAccountDetails(Account account) {
+        System.out.println("code for updating an exisiting account object in database");
+        return null;
+    }
+
+    public boolean deleteAccountDetails(Account account) {
+        System.out.println("Delete account from database");
+        return true;
+    }
+}
---
+++ src/com/upGrad/service/AccountServiceImpl.java
@@ -0,0 +1,57 @@
+package com.upGrad.service;
+
+import com.upGrad.DAO.AccountDAOImpl;
+import com.upGrad.beans.Account;
+
+public class AccountServiceImpl {
+
+    AccountDAOImpl accountDAO = new AccountDAOImpl();
+
+    public Account acceptAccountDetails(Account account) {
+        return accountDAO.acceptAccountDetails(account);
+    }
+
+    public Account getAccountDetails(int accountNumber, int pin) {
+        Account myAccount = accountDAO.getAccountDetails(accountNumber);
+        if (myAccount == null) {
+            System.out.println("Account does not exists");
+            return null;
+        } else {
+            if (myAccount.getPin() == pin) {
+                System.out.println("Details are correct : login to account");
+                return myAccount;
+            } else {
+                System.out.println("Incorrect pin ");
+                return null;
+            }
+        }
+    }
+
+    public Account depositAmount(int accountNumber, int amount) {
+        Account account = accountDAO.getAccountDetails(accountNumber);
+        account.setCurrentBalance(account.getCurrentBalance() + amount);
+        accountDAO.updateAccountDetails(account);
+        return account;
+    }
+
+    public Account withdrawAmount(int accountNumber, int pin, int amount) {
+        Account myAccount = getAccountDetails(accountNumber, pin);
+        if (myAccount == null) {
+            return null;
+        } else {
+            int currentBalance = myAccount.getCurrentBalance();
+            if (currentBalance < amount) {
+                System.out.println("Not enough balance");
+                return myAccount;
+            } else {
+                myAccount.setCurrentBalance(myAccount.getCurrentBalance() - amount);
+                return accountDAO.updateAccountDetails(myAccount);
+            }
+        }
+    }
+
+    public boolean deleteAccount(Account account) {
+        return accountDAO.deleteAccountDetails(account);
+    }
+
+}
