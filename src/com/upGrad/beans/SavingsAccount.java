5652d067955cdf72ab25dcad8129551165deed01
sndpoffcl <sandeep2official@gmail.com>
2020-06-29 06:15:18 +0530

    session2 slide 97 handsOn

---
+++ src/com/upGrad/beans/SavingsAccount.java
@@ -0,0 +1,19 @@
+package com.upGrad.beans;
+
+public class SavingsAccount extends Account {
+    private boolean freeTransactions;
+
+    public SavingsAccount(String accountHolderName, int accountNumber, int currentBalance, int pin) {
+        super(accountHolderName, accountNumber, currentBalance, pin);
+    }
+
+    public boolean isFreeTransactions() {
+        return freeTransactions;
+    }
+
+    public void setFreeTransactions(boolean freeTransactions) {
+        this.freeTransactions = freeTransactions;
+    }
+
+    
+}
