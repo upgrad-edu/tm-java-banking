67ca571e92ee6368ce56c1dc0a60b7cbfca8e973
sndpoffcl <sandeep2official@gmail.com>
2020-06-29 12:06:14 +0530

    session 3 slide 21 handsOn

--- src/com/upGrad/utils/EMICalculator.java
+++ src/com/upGrad/utils/EMICalculator.java
@@ -3,6 +3,12 @@ package com.upGrad.utils;
 public class EMICalculator {
     public static int calculateEMIAmount(int totalAmount, int months){
         //we will add emi calculation functionality here
+        int emiAmount = 0;
+        try{
+            emiAmount = totalAmount/months;
+        }catch (ArithmeticException ae){
+            System.out.println("Number of months can't be less than 1");
+        }
         return 0;
     }
 
