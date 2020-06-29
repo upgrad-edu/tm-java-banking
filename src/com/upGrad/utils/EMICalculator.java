7697d8415d3804aa0802e60c26f863d467207adc
sndpoffcl <sandeep2official@gmail.com>
2020-06-29 12:33:15 +0530

    Session 3 slide 64 handsOn

--- src/com/upGrad/utils/EMICalculator.java
+++ src/com/upGrad/utils/EMICalculator.java
@@ -1,24 +1,32 @@
 package com.upGrad.utils;
 
 public class EMICalculator {
-    public static int calculateEMIAmount(int totalAmount, int months){
+    public static int calculateEMIAmount(int totalAmount, int months) {
         //we will add emi calculation functionality here
         int emiAmount = 0;
-        try{
-            emiAmount = totalAmount/months;
-        }catch (ArithmeticException ae){
+        try {
+            emiAmount = totalAmount / months;
+        } catch (ArithmeticException ae) {
             System.out.println("Number of months can't be less than 1");
+        } catch (Exception e) {
+            System.out.println("Exception has been caught");
+        } finally {
+            System.out.println("Thanks for using our service");
         }
         return emiAmount;
     }
 
-    public static int calculateNumberOfEMIs(int totalAmount, int emiAmount){
+    public static int calculateNumberOfEMIs(int totalAmount, int emiAmount) {
         //we will add emi calculation functionality here
         int emiPayments = 0;
-        try{
-            emiPayments = totalAmount/emiAmount;
-        }catch (ArithmeticException ae){
-            System.out.println("Emi amount cant be less than 1");
+        try {
+            emiPayments = totalAmount / emiAmount;
+        } catch (ArithmeticException ae) {
+            System.out.println("Thanks for using our service");
+        } catch (Exception e) {
+            System.out.println("Exception has been caught");
+        } finally {
+            System.out.println("Thanks for using our service");
         }
         return emiPayments;
     }
